4a364b02b69b81bdd33d559301d84d9f9f9e5a7b
AnboYukari <b1018129@gmail.com>
2020-06-29 16:29:54 +0900

    sweetのAPI作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :shops
+
+  resources :shops do
+    resources :sweets, only: %i[index create]
+  end
+
   resources :categories
+
+  resources :sweets, except: :create
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
